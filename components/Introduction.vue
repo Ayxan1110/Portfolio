5ab63efa15e7d28a2f1981a3bc8f0deb6365b32c
Ayxan1110 <ayxan110.dadashov@gmail.com>
2024-10-19 21:50:08 +0400

    typing animation added

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -13,6 +13,23 @@
 .asas{
     font-family: "SF Mono";
     color: var(--color);
+    width: 15ch;
+    animation: typing 2s steps(22), blink .5s step-end infinite alternate;
+    white-space: nowrap;
+    overflow: hidden;
+    border-right: 3px solid;
+}
+
+@keyframes typing {
+  from {
+    width: 0
+  }
+}
+    
+@keyframes blink {
+  50% {
+    border-color: transparent
+  }
 }
 .name{
     color: #fff;
