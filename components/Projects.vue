b46166f8220f2ccb187b60bfda09bdba7e7e47bb
Ayxan1110 <ayxan110.dadashov@gmail.com>
2024-11-01 03:03:09 +0200

    project added

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,14 +1,54 @@
+<script>
+export default {
+  data() {
+    return {
+      tictac: {
+        linkUrl: "https://ayxan1110.github.io/TicTacToeJS/",
+        repoUrl: "https://github.com/Ayxan1110/TicTacToeJS/"
+      },
+      swp: {
+        linkUrl: "",
+        repoUrl: ""
+      }
+    };
+  },
+  methods: {
+    redirectToPage(url) {
+        window.open(url, "_blank");
+    }
+  }
+}
+</script>
 <template>
     <div id="projects" class="md:py-36 sm: py-12">
         <div>
             <h3 class="section-header"><span class="nums-content">03.</span>My Projects</h3>
         </div>
         <div class="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 mt-20 gap-4">
+            <div @click="redirectToPage(tictac.linkUrl)" class="project-wrap">
+                <div class="project">
+                    <div class="flex justify-between items-center">
+                        <img width="40" height="40" src="../public/folder.svg" alt="folder">
+                        <a class="gh-icon" :href="tictac.repoUrl" target="_blank" @click.stop><Github /></a>
+                    </div>
+                    <div class="mt-6">
+                        <h3 class="project-name">Tic Tac Toe</h3>
+                        <p class="project-desc">Classic Tic-Tac-Toe game built with jQuery</p>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>jQuery</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
             <div class="project-wrap">
                 <div class="project">
                     <div class="flex justify-between items-center">
                         <img width="40" height="40" src="../public/folder.svg" alt="folder">
-                        <a style="height: 20px; width: 20px;" href="#"><img src="../public/github.svg" alt="folder"></a>
+                        <a class="gh-icon" target="_blank" @click.stop><Github /></a>
                     </div>
                     <div class="mt-6">
                         <h3 class="project-name">Coming soon...</h3>
@@ -27,26 +67,7 @@
                 <div class="project">
                     <div class="flex justify-between items-center">
                         <img width="40" height="40" src="../public/folder.svg" alt="folder">
-                        <a style="height: 20px; width: 20px;" href="#"><img src="../public/github.svg" alt="folder"></a>
-                    </div>
-                    <div class="mt-6">
-                        <h3 class="project-name">Coming soon...</h3>
-                        <p class="project-desc">In a short period of time there will be valid projects</p>
-                    </div>
-                    <div>
-                        <ul>
-                            <li>Just</li>
-                            <li>Wait</li>
-                            <li>)</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="project-wrap">
-                <div class="project">
-                    <div class="flex justify-between items-center">
-                        <img width="40" height="40" src="../public/folder.svg" alt="folder">
-                        <a style="height: 20px; width: 20px;" href="#"><img src="../public/github.svg" alt="folder"></a>
+                        <a class="gh-icon" target="_blank" @click.stop><Github /></a>
                     </div>
                     <div class="mt-6">
                         <h3 class="project-name">Coming soon...</h3>
@@ -103,6 +124,19 @@ ul{
     padding: 0px;
     margin: 20px 0px 0px;
     list-style: none;
+}
+.gh-icon{
+    color: #fff !important;
+    transition: all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1);
+    z-index: 11;
+}
+.gh-icon svg{
+    width: 25px;
+    height: 25px;
+}
+.gh-icon:hover svg{
+    stroke: var(--color);
+    transition-duration: .3s;
 }
 </style>
   
