034e161419cb6b933f0f19d42c9b4f13a23756c4
Ayxan1110 <ayxan110.dadashov@gmail.com>
2024-11-06 15:48:52 +0200

    return fixed issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
-export default {
+export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss'],
   components: [
     {
       path: '~/components',
-      pathPrefix: false,
+      pathPrefix: false,    
     },
   ],
   css: [
@@ -18,8 +18,8 @@ export default {
       viewport: 'width=device-width, initial-scale=1',
       title: 'Aykhan Dadashov',
       meta: [
-        { name: 'description', content: 'Portfolio website of Aykhan Dadashov' }
+        { name: 'description', content: 'Portflio website of Aykhan Dadashov' }
       ],
     },
   }
-};
+})
